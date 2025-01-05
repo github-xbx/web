214edfab1780e5c70717b6ae9ddc92c6f488a3a3
xing <3154401838@qq.com>
2025-01-05 18:55:18 +0800

    456

---
+++ src/java/com/example/controller/JenkinsController.java
@@ -0,0 +1,33 @@
+package com.example.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/jenkins")
+public class JenkinsController {
+
+
+
+
+    @GetMapping("select")
+    public Map<String, Object> select(){
+
+        Map<String, Object> map = new HashMap<>();
+
+        map.put("list", Arrays.asList("haha","aaa","eeee","abcd"));
+        map.put("string","jenkins 流水线构建");
+        map.put("integer",123);
+
+        return map;
+
+
+    }
+
+
+}
